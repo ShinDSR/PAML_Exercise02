16e2b0363a5aeda5623405a025079d3506ae571b
ShinDSR <danang.wibisono.ft20@mail.umy.ac.id>
2023-06-12 01:18:15 +0700

    edit ui on add_contact & edit_contact

--- lib/view/add_contact.dart
+++ lib/view/add_contact.dart
@@ -25,62 +25,109 @@ class _AddContactState extends State<AddContact> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Contact'),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Form(
-          key: formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(hintText: 'Name'),
-                onChanged: (value) {
-                  name = value;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: 'Phone'),
-                onChanged: (value) {
-                  phone = value;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: 'Email'),
-                onChanged: (value) {
-                  email = value;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: 'Address'),
-                onChanged: (value) {
-                  address = value;
-                },
-              ),
-              TextButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      foregroundColor: MaterialStateProperty.all(Colors.white)),
-                  onPressed: () {
-                    ContactModel cm = ContactModel(
-                      name: name!,
-                      phone: phone!,
-                      email: email!,
-                      address: address!,
-                    );
-                    contactController.addContact(cm);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Contact Changed')));
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Contact(),
-                        ));
-                  },
-                  child: Text('add'))
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF000000),
+              Color(0xFF1A237E),
+              Color(0xFF673AB7),
             ],
           ),
         ),
-      ),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onChanged: (value) {
+                    name = value;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Phone',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onChanged: (value) {
+                    phone = value;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Address',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onChanged: (value) {
+                    address = value;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        foregroundColor: MaterialStateProperty.all(Colors.white)),
+                    onPressed: () {
+                      ContactModel cm = ContactModel(
+                        name: name!,
+                        phone: phone!,
+                        email: email!,
+                        address: address!,
+                      );
+                      contactController.addContact(cm);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Contact Changed')));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Contact(),
+                          ));
+                    },
+                    child: Text('Add'))
+              ],
+            ),
+          ),
+        ),
+      )
     );
   }
 }
--- lib/view/edit_contact.dart
+++ lib/view/edit_contact.dart
@@ -37,71 +37,118 @@ class _EditContactState extends State<EditContact> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Update data'),
+        backgroundColor: Colors.grey,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Form(
-          key: formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Name'),
-                onSaved: (value) {
-                  tempname = value;
-                },
-                initialValue: widget.name,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Phone'),
-                onSaved: (value) {
-                  tempphone = value;
-                },
-                initialValue: widget.phone,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Email'),
-                onSaved: (value) {
-                  tempmail = value;
-                },
-                initialValue: widget.email,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'address'),
-                onSaved: (value) {
-                  tempaddress = value;
-                },
-                initialValue: widget.address,
-              ),
-              TextButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      foregroundColor: MaterialStateProperty.all(Colors.white)
-                      ),
-                  onPressed: () {
-                    if (formkey.currentState!.validate()) {
-                      formkey.currentState!.save();
-                      ContactModel cm = ContactModel(
-                          id: widget.id,
-                          name: tempname!.toString(),
-                          phone: tempphone!.toString(),
-                          email: tempmail!.toString(),
-                          address: tempaddress!.toString());
-                      contactController.updateContact(cm);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Contact Changed')));
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Contact(),
-                        ),
-                      );
-                    }
-                  },
-                  child: Text('edit'))
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF000000),
+              Color(0xFF1A237E),
+              Color(0xFF673AB7),
             ],
           ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onSaved: (value) {
+                    tempname = value;
+                  },
+                  initialValue: widget.name,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Phone',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onSaved: (value) {
+                    tempphone = value;
+                  },
+                  initialValue: widget.phone,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onSaved: (value) {
+                    tempmail = value;
+                  },
+                  initialValue: widget.email,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Address',
+                    border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white
+                  ),
+                  onSaved: (value) {
+                    tempaddress = value;
+                  },
+                  initialValue: widget.address,
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        foregroundColor: MaterialStateProperty.all(Colors.white)
+                        ),
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        formkey.currentState!.save();
+                        ContactModel cm = ContactModel(
+                            id: widget.id,
+                            name: tempname!.toString(),
+                            phone: tempphone!.toString(),
+                            email: tempmail!.toString(),
+                            address: tempaddress!.toString());
+                        contactController.updateContact(cm);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Contact Changed')));
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Contact(),
+                          ),
+                        );
+                      }
+                    },
+                    child: Text('Edit'))
+              ],
+            ),
+          ),
+        )
       ),
     );
   }
